76f2789abb6a977d647edd7b70524ba41fdef192
weitaohu <824784596@qq.com>
2025-04-16 13:33:32 -0700

    Complete the assignment of the sequence

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,10 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SudokuBoard {
-   private int[][] board;
-   
-   public SudokuBoard(String filename) {
-   }
-   
-   public String toString() {
-      return "";
-   }
-}
+    private char[][] board;
+
+    public SudokuBoard(String filename) {
+        board = new char[9][9];  
+        try {
+            Scanner input = new Scanner(new File(filename));
+            for (int i = 0; i < 9; i++) {
+                String line = input.nextLine();
+                for (int j = 0; j < 9; j++) {
+                    board[i][j] = line.charAt(j);  
+                }
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+
