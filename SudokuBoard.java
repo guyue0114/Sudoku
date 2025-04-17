415dded8f288d5e17c9f0b1547c90085f73857d0
weitaohu <824784596@qq.com>
2025-04-17 12:24:52 -0700

    tostring has been modified
    The hard-coding issue of tostring has been modified

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -3,38 +3,42 @@ import java.util.*;
 
 public class SudokuBoard {
     private char[][] board;
-    board = new char[9][9];
 
     public SudokuBoard(String filename) {
-        
         try {
+            List<String> lines = new ArrayList<>();
             Scanner scanner = new Scanner(new File(filename));
-            int row = 0;
-            while (scanner.hasNextLine() && row < 9) {
-                String line = scanner.nextLine();
-                for (int col = 0; col < 9; col++) {
-                    board[row][col] = line.charAt(col);
-                }
-                row++;
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.nextLine());
             }
             scanner.close();
+
+            int numRows = lines.size();
+            int numCols = lines.get(0).length(); 
+            board = new char[numRows][numCols];
+
+            for (int row = 0; row < numRows; row++) {
+                String line = lines.get(row);
+                for (int col = 0; col < numCols; col++) {
+                    board[row][col] = line.charAt(col);
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + filename);
         }
     }
 
-   
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int row = 0; row < 9; row++) {
+        for (int row = 0; row < board.length; row++) {
             if (row % 3 == 0 && row != 0) {
                 sb.append("------+-------+------\n");
             }
-            for (int col = 0; col < 9; col++) {
+            for (int col = 0; col < board[row].length; col++) {
                 if (col % 3 == 0 && col != 0) {
                     sb.append("| ");
                 }
-                sb.append(board[row][col] + " ");
+                sb.append(board[row][col]).append(" ");
             }
             sb.append("\n");
         }
